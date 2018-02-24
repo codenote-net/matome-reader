19ec7cb7df465ecc99f68f5d458921f6c7b43157
Tadashi Shigeoka <shigeoka@codenote.net>
2018-02-24 22:49:53 +0900

    :+1: Open the url in the home page using InAppBrowser
    - https://ionicframework.com/docs/native/in-app-browser/
    - https://github.com/apache/cordova-plugin-inappbrowser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -26,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ListPage
   ],
   providers: [
+    InAppBrowser,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
