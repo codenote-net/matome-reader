decabd8da0e0a0d801a8dcc043543b1345480bad
Tadashi Shigeoka <shigeoka@codenote.net>
2018-03-26 14:02:30 +0900

    :+1: Added pull-to-refresh (but only UI)
    - [Refresher - Ionic API Documentation - Ionic Framework](https://ionicframework.com/docs/api/components/refresher/Refresher/)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -57,6 +57,15 @@ export class HomePage {
     });
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
   doInfinite(infiniteScroll) {
     const cursor = this.getCursor();
 
